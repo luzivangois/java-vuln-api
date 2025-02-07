3a4e2610afc0151bde85af7ba6335efd8f2af2b8
luzivangois <luzivangois@outlook.com>
2025-02-07 11:56:56 -0300

    código teste para a API recuperar o token JWT a partir do cookie

--- src/main/java/com/advocacia/api/infra/security/SecurityFilter.java
+++ src/main/java/com/advocacia/api/infra/security/SecurityFilter.java
@@ -3,6 +3,7 @@ package com.advocacia.api.infra.security;
 import com.advocacia.api.repositories.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -28,9 +30,20 @@ public class SecurityFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request){
-        var authHeader = request.getHeader("Authorization");
-        if(authHeader == null) return null;
-        return authHeader.replace("Bearer ", "");
+//    private String recoverToken(HttpServletRequest request){
+//        var authHeader = request.getHeader("Authorization");
+//        if(authHeader == null) return null;
+//        return authHeader.replace("Bearer ", "");
+//    }
+    private String recoverToken(HttpServletRequest request) {
+        // Recupera o token diretamente do cookie "auth-token"
+        if (request.getCookies() != null) {
+            return Arrays.stream(request.getCookies())
+                    .filter(cookie -> "auth-token".equals(cookie.getName()))
+                    .map(Cookie::getValue)
+                    .findFirst()
+                    .orElse(null);
+        }
+        return null;
     }
 }

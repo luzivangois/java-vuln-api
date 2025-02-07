d4db04f4b04c325e4a309a5730c58a7488f79b95
luzivangois <luzivangois@outlook.com>
2025-02-06 22:55:17 -0300

    Alterada regra de CORS, para especificar origen csrf-attack e p metodo AllowedOrigins

--- src/main/java/com/advocacia/api/infra/security/CorsConfig.java
+++ src/main/java/com/advocacia/api/infra/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(
+                .allowedOrigins(
                         "http://localhost:4200",
                         "http://localhost:4300",
                         "https://advocacia-associados.netlify.app",

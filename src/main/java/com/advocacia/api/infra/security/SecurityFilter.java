d7bbb7758cb72465416e398ec822906c02645c2f
luzivangois <luzivangois@outlook.com>
2025-02-07 16:12:00 -0300

    desfeita mudanças na forma que o endpoint de login gera o token e na forma que a api usa o token enviado. mantido a recuperação do token a partir do headers

--- src/main/java/com/advocacia/api/infra/security/SecurityFilter.java
+++ src/main/java/com/advocacia/api/infra/security/SecurityFilter.java
@@ -3,7 +3,6 @@ package com.advocacia.api.infra.security;
 import com.advocacia.api.repositories.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
-import java.util.Arrays;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -30,20 +28,9 @@ public class SecurityFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-//    private String recoverToken(HttpServletRequest request){
-//        var authHeader = request.getHeader("Authorization");
-//        if(authHeader == null) return null;
-//        return authHeader.replace("Bearer ", "");
-//    }
-    private String recoverToken(HttpServletRequest request) {
-        // Recupera o token diretamente do cookie "auth-token"
-        if (request.getCookies() != null) {
-            return Arrays.stream(request.getCookies())
-                    .filter(cookie -> "auth-token".equals(cookie.getName()))
-                    .map(Cookie::getValue)
-                    .findFirst()
-                    .orElse(null);
-        }
-        return null;
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
     }
 }

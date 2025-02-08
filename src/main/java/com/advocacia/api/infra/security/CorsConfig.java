ebf38a3fa666f794c48ac9c2174a3e10fb7237e7
luzivangois <luzivangois@outlook.com>
2025-02-07 23:10:04 -0300

    testando allowedOriginPatterns(*) com AllowedCredentials True

--- src/main/java/com/advocacia/api/infra/security/CorsConfig.java
+++ src/main/java/com/advocacia/api/infra/security/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
